b6d452055737aeb41f7db0b20056097ab0c1cf8a
Nuno Melo <67382746+nunomelo98@users.noreply.github.com>
2025-05-21 10:23:21 -0100

    conf:initial configuration of the project

---
+++ src/main/java/com/nm/garagemanager/GaragemanagerApplication.java
@@ -0,0 +1,13 @@
+package com.nm.garagemanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GaragemanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GaragemanagerApplication.class, args);
+	}
+
+}
