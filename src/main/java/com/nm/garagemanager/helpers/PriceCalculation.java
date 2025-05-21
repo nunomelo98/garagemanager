15d2691077b9cd5d036afeb9f97b0672aa0e6e92
Nuno Melo <67382746+nunomelo98@users.noreply.github.com>
2025-05-21 17:53:55 -0100

    feat:creaate the price calculation price

---
+++ src/main/java/com/nm/garagemanager/helpers/PriceCalculation.java
@@ -0,0 +1,19 @@
+package com.nm.garagemanager.helpers;
+
+public class PriceCalculation {
+    public double calculatePrice(double basePrice, int currentOccupancy, int maxCapacity){
+        double occupancyRate = (double) currentOccupancy / maxCapacity;
+
+        if(occupancyRate < 0.25){
+            return basePrice *0.9;
+        }else if (occupancyRate < 0.5) {
+            return basePrice;
+
+        }else if (occupancyRate < 0.75) {
+            return basePrice * 1.1;
+        }else{
+            return basePrice * 1.25;
+        }
+    }
+
+}
